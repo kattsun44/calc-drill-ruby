059cd598803f6574c615715aab86f2e628271f37
勝原 祥大 <yoshihiro.katsuhara@oro.com>
2021-12-21 12:49:48 +0900

    複数問解ける&時間計測

--- calc.rb
+++ calc.rb
@@ -1,11 +1,23 @@
-operand1 = rand(100)
-operand2 = rand(100)
-answer = operand1 + operand2
-puts "#{operand1} + #{operand2} = ?"
+print "How many do you want to solve?\n> "
+q = gets.to_i
+count = 0
+start_time = Time.now
 
-input = gets.to_i
-while input != answer
-  puts "not good..."
+while count != q
+  operand1 = rand(100)
+  operand2 = rand(100)
+  answer = operand1 + operand2
+  print "Q#{count + 1}. #{operand1} + #{operand2} = ?\n> "
+
   input = gets.to_i
+  while input != answer
+    puts "not good..."
+    input = gets.to_i
+  end
+  puts "right!"
+  count += 1
 end
-puts "right!"
+
+solved_time = Time.now - start_time
+
+puts "Total: #{count}, Time: #{solved_time}s"
